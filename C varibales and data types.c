f11d635cd72bf1be729023a19a273b00751f77ba
Arnold-Bcs <herculees093@gmail.com>
2024-09-22 18:21:39 +0300

    Create C varibales and data types.c

---
+++ C varibales and data types.c
@@ -0,0 +1,19 @@
+//C variables and data types
+//preprocessor directive
+#include <stdio.h>
+
+int main() {
+  //declaration and initialisation
+  char a='k';
+  char name[]="keith";
+  int age=10;
+  float area=2.035;
+  double salary=2000.60;
+printf("The character is %c\n",a);
+printf("The string is %s\n",name);
+printf("The age is %d\n",age);
+printf("The area is %f\n",area);
+printf("The salary is %f\n",salary);
+
+    return 0;
+}
